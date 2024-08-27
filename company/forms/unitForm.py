fa0f1cb75d37cf06c496ce87befc6e197a467711
edmond.mutabazi <mufasheonline@gmail.com>
2024-08-27 22:00:25 +0200

    27/08/2024_changes

--- company/forms/unitForm.py
+++ company/forms/unitForm.py
@@ -12,18 +12,18 @@ class UnitForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'placeholder': 'Enter email'})
         }
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     if self.instance.company is not None:
-    #         self.fields['department'].queryset = self.instance.company.department_set.order_by('name')
-    #     else:
-    #         self.fields['department'].queryset = Department.objects.none()
-    #     if 'company' in self.data:
-    #         try:
-    #             company_id = int(self.data.get('company'))
-    #             self.fields['department'].queryset = Department.objects.filter(company_id=company_id).order_by('name')
-    #         except(ValueError, TypeError):
-    #             pass
-    #
-    #     elif self.instance.pk and self.instance.company is not None:
-    #         self.fields['department'].queryset = self.instance.company.department_set.order_by('name')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.company is not None:
+            self.fields['department'].queryset = self.instance.company.department_set.order_by('name')
+        else:
+            self.fields['department'].queryset = Department.objects.none()
+        if 'company' in self.data:
+            try:
+                company_id = int(self.data.get('company'))
+                self.fields['department'].queryset = Department.objects.filter(company_id=company_id).order_by('name')
+            except(ValueError, TypeError):
+                pass
+
+        elif self.instance.pk and self.instance.company is not None:
+            self.fields['department'].queryset = self.instance.company.department_set.order_by('name')
